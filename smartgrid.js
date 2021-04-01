66f533a9a11ccf087ece4b520ed1d0521f8ae6ec
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-04-01 12:43:51 +0300

    Initial commit

---
+++ smartgrid.js
@@ -0,0 +1,23 @@
+module.exports = {
+  columns: 12,
+    offset: '30px', //columns
+    container: {
+      maxWidth: '1140px',
+        fields: '20px' // container
+      },
+      breakPoints: {
+        // xl: {
+        //   width: '1200px'
+        // },
+        // lg: {
+        //  width: '992px'
+        // },
+        // md: {
+        //  width: '768px'
+        // },
+        xs: {
+         width: '375px'
+        }
+      },
+     // detailedCalc: true
+   };
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//= ../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js
+
+$(function(){
+
+	$('input[type="tel"]').mask("+7 (999) 999-9999");
+
+
+});
+
